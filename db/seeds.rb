ea43a78e06b12a737efaeba59d7f2b370367bdfc
Dwire <gjdwyer8@gmail.com>
2018-04-10 18:43:41 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@ Post.create!(
   content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dapibus, nulla vel condimentum ornare, arcu lorem hendrerit purus, ac sagittis ipsum nisl nec erat. Morbi porta sollicitudin leo, eu cursus libero posuere ac. Sed ac ultricies ante. Donec nec nulla ipsum. Nunc eleifend, ligula ut volutpat.",
   category: "Non-Fiction"
 )
+
+
+Author.create!(
+  name: "Adad Lovelace", email: "adad@the-history-of-programming.com"
+)
+
+Post.create!(
+  title: "How to Science",
+  content: "arcu lorem hendrerit purus, ac sagittis ipsum nisl nec erat. Morbi porta sollicitudin leo, eu cursus libero posuere ac. Sed ac ultricies ante. Donec nec nulla ipsum. Nunc eleifend, ligula",
+  category: "Fiction"
+)
